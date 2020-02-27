64afe505e491e55602ec3e8c744df753d32f51a7
Neha Pandey <nehapandey@Nehas-MacBook-Pro.local>
2020-02-28 01:11:54 +0530

    CalculatorNeha program

--- src/main/java/com/tw/workshop/CalculatorNeha.java
+++ src/main/java/com/tw/workshop/CalculatorNeha.java
@@ -1,18 +1,50 @@
 package com.tw.workshop;
 
-public class Calculator_Neha {
+public class CalculatorNeha {
 
-    public void sum() {
+   public int number1 ;
+   public int number2 ;
+   public static int result ;
+
+
+    public int sum(int number1, int number2) {
+        this.number1 =number1 ;
+        this.number2= number2;
+     return result = number1+number2  ;
 
     }
 
-    public void subtratct() {
+    public int subtract(int number1, int number2) {
+        this.number1 =number1 ;
+        this.number2= number2;
+        return result = number1-number2  ;
 
     }
 
-    public void divide() {
+    public int division(int number1, int number2) {
+        this.number1 =number1 ;
+        this.number2= number2;
+        return result = number1/number2  ;
 
     }
 
-    public void multiply() {}
+    public int multiplication(int number1, int number2) {
+        this.number1 =number1 ;
+        this.number2= number2;
+        return result = number1*number2  ;
+
+    }
+
+    public static void main (String [] args) {
+        CalculatorNeha calc = new CalculatorNeha();
+         calc.sum(15,5);
+        System.out.println ("Sum of the numbers is " +result );
+        calc.subtract(12,4);
+        System.out.println ("Subtraction of the numbers is " +result );
+        calc.division(21,3);
+        System.out.println ("Division of the numbers is " +result );
+        calc.multiplication(6,3);
+        System.out.println ("Multiplication of the numbers is " +result );
+
+    }
 }
